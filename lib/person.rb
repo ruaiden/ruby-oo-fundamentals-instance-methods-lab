953960eabcdfec9948b76295c044db7099822b9a
Aiden <aiden.marques94@gmail.com>
2020-11-24 10:34:59 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def instance
+        talk
+    end
+
+    def talk
+        puts "Hello World!"
+    end 
+
+    def walk
+        puts "The Person is walking"
+    end 
+end 
+
+Person.new
